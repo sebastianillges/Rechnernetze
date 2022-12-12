83f23d11b4dc343cffa4f7e4c9fa999d37790f4c
kaicikoglu <kaicikoglu@outlook.de>
2022-12-12 21:06:43 +0100

    5.2

--- Praktikum/Socketprogrammierung/PortScanner/tcpScan.py
+++ Praktikum/Socketprogrammierung/PortScanner/tcpScan.py
@@ -1,37 +1,31 @@
-import threading
-from socket import AF_INET
-from socket import SOCK_STREAM
-from socket import socket
+from socket import SOCK_STREAM, AF_INET, socket
 from threading import Thread
-
-CONTINUE = True
+from time import sleep
 
 
 # returns True if a connection can be made, False otherwise
 def test_port_number(host, port):
-    # create and configure the socket
-    with socket(AF_INET, SOCK_STREAM) as sock:
-        # set a timeout of a few seconds
-        sock.settimeout(3)
-        # connecting may fail
-        try:
-            # attempt to connect
-            sock.connect((host, port))
-            # a successful connection was made
-            return True
-        except:
-            # ignore the failure
-            return False
+    # create a socket
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.settimeout(2)
+    # try to connect to the host
+    try:
+        print(sock.connect_ex((host, port)))
+        return True
+    except:
+        return False
 
 
 # scan port numbers on a host
 def port_scan(host, port):
-    print(f'Scanning {host}...')
     # scan each port number
     if test_port_number(host, port):
         print(f'> {host}:{port} open')
 
 
-for i in range(1, 100):
+print(f'Scanning 141.37.168.26...')
+for i in range(1, 51):
+    # create a thread for each port number
+    sleep(0.1)
     t = Thread(target=port_scan, args=('141.37.168.26', i))
     t.start()
--- Praktikum/Socketprogrammierung/PortScanner/udpScan.py
+++ Praktikum/Socketprogrammierung/PortScanner/udpScan.py
@@ -1,62 +1,25 @@
-import random
 import socket
-import struct
+import time
 from threading import Thread
 
+Server_IP = '141.37.168.26'
+MESSAGE = 'Hello, World!'
 
-class SendDNSPkt:
-    def __init__(self, url, serverIP, port=1):
-        self.url = url
-        self.serverIP = serverIP
-        self.port = port
 
-    def sendPkt(self):
-        pkt = self._build_packet()
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(1)
-        sock.sendto(bytes(pkt), (self.serverIP, self.port))
+def scan_port(port):
+    print('Sending message', MESSAGE, 'to UDP server with IP ', Server_IP, ' on Port=', port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(10)
+    sock.sendto(MESSAGE.encode('utf-8'), (Server_IP, port))
+    try:
         data, addr = sock.recvfrom(1024)
-        sock.close()
-        return data
-
-    def _build_packet(self):
-        randint = random.randint(1, 50)
-        packet = struct.pack(">H", randint)  # Query Ids (Just 1 for now)
-        packet += struct.pack(">H", 0x0100)  # Flags
-        packet += struct.pack(">H", 1)  # Questions
-        packet += struct.pack(">H", 0)  # Answers
-        packet += struct.pack(">H", 0)  # Authorities
-        packet += struct.pack(">H", 0)  # Additional
-        split_url = self.url.split(".")
-        for part in split_url:
-            packet += struct.pack("B", len(part))
-            for s in part:
-                packet += struct.pack('c', s.encode())
-        packet += struct.pack("B", 0)  # End of String
-        packet += struct.pack(">H", 1)  # Query Type
-        packet += struct.pack(">H", 1)  # Query Class
-        return packet
+        print('received message: ' + data.decode('utf-8') + ' from ', addr)
+    except socket.timeout:
+        print('Socket timed out at', time.asctime())
+    sock.close()
 
 
-def checkDNSPortOpen(port):
-    # replace 8.8.8.8 with your server IP!
-
-    s = SendDNSPkt('www.google.com', '141.37.168.26', port)
-    portOpen = False
-    for _ in range(5):  # udp is unreliable.Packet loss may occur
-        try:
-            s.sendPkt()
-            portOpen = True
-            break
-        except socket.timeout:
-            pass
-    if portOpen:
-        print('port open!')
-    else:
-        print('port closed!')
-
-
-if __name__ == '__main__':
-    for i in range(1, 50):
-        t = Thread(target=checkDNSPortOpen, args=(i,))
-        t.start()
+for i in range(1, 51):
+    time.sleep(0.1)
+    t = Thread(target=scan_port, args=(i,))
+    t.start()
