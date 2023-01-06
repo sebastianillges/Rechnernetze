10327027a0eadffaa9b73d1cd157337e1595043f
sebastianillges <sebastian.illges@gmail.com>
2023-01-07 00:54:04 +0100

    ich Hase mein Leben aber GoBackNSocket.py sollte jetzt stimmen

--- Praktikum/Go-Back-N/GoBackNSocket.py
+++ Praktikum/Go-Back-N/GoBackNSocket.py
@@ -1,10 +1,11 @@
-import socket
-
-import socket
-from threading import Thread
-import random
+import time
 from math import ceil
 from lossy_udp_socket import lossy_udp_socket
+
+
+def idToInt(id):
+    while id.startswith("0") and len(id) > 1: id = id[1:]
+    return int(id)
 
 
 class GoBackNSocket():
@@ -16,26 +17,72 @@ class GoBackNSocket():
         self.remoteAddress = remoteAddress
         self.sock = lossy_udp_socket(self, localPort, (remoteAddress, remotePort), PLR)
         self.segmentSize = segmentSize
+        if self.segmentSize <= 4:
+            self.segmentSize = 5
         self.windowSize = windowSize
 
 
     def send(self, msg):
         length = len(msg)
-        numSegments = ceil(length/ self.segmentSize)
-        segments = []
-        for i in range(0, numSegments):
-            segments.append(msg[i * self.segmentSize : (i+1) * self.segmentSize])
-        while(GoBackNSocket.currentSeg < numSegments)
+        numPackages = ceil(length / (self.segmentSize - self.headerSize))
+        p = []
+        for i in range(0, numPackages):
+            id = str(i).zfill(self.headerSize)
+            p.append(package(
+                id + msg[i * (self.segmentSize - self.headerSize): (i + 1) * (self.segmentSize - self.headerSize)]))
+        while self.currentPackage < numPackages:
+            # print(str(self.currentPackage) + ", " + str(self.expectedPackage))
+            # print(str(time.time()) + ", " + str(self.t + self.timeout))
+            sleep(0.05)
+            # print(f"current = {GoBackNSocket.currentPackage}, expected = {GoBackNSocket.expectedPackage}")
+            if time.time() > self.t + self.timeout:
+                self.pprint(f"Package: {str(GoBackNSocket.expectedPackage)} timed out")
+                GoBackNSocket.currentPackage = GoBackNSocket.expectedPackage
 
-    def receive(self, packet):
+            if GoBackNSocket.currentPackage <= self.windowMax:
+                self.sock.send(p[GoBackNSocket.currentPackage].data.encode("utf-8"))
+                self.pprint(f"Sent package: {str(p[GoBackNSocket.currentPackage].id)} "
+                            f"pkg-data: {str(p[GoBackNSocket.currentPackage].data[self.headerSize:])}")
+                sleep(0.001)
+                self.t = time.time()
+                GoBackNSocket.currentPackage += 1
+
+    def receive(self, msg):
+        msg = msg.decode('utf-8')
+        id = idToInt(msg[:4])
+        self.pprint("Received ack: " + str(id))
+        if id == GoBackNSocket.expectedPackage:
+            GoBackNSocket.expectedPackage += 1
+            if GoBackNSocket.currentPackage == GoBackNSocket.windowMax - 1:
+                GoBackNSocket.windowMax = id + self.windowSize
+
+    def pprint(self, text):
+        string = text.ljust(60, ' ')
+        string = string + str(f"current = {GoBackNSocket.currentPackage}, "
+                              f"expected = {GoBackNSocket.expectedPackage}, "
+                              f"windowMax = {self.windowMax}")
+        print(string)
+        sleep(0.001)
+
+
+class package():
+    def __init__(self, data):
+        self.data = data
+        self.id = idToInt(data[:4])
+
 
 if __name__ == '__main__':
     serverPort = 12000
     clientPort = 12001
-    serverSocket = GoBackNSocket(serverPort, clientPort, "localhost")
+    serverAdress = "127.0.0.1"
+    clientAdress = "172.29.224.1"
 
-    clientSocket = GoBackNSocket(clientPort, serverPort, "127.0.0.96")
-    clientSocket.send("ichbingay")
+    serverSocket = GoBackNSocket(serverPort, clientPort, serverAdress, 0.1)
+    clientSocket = GoBackNSocket(clientPort, serverPort, serverAdress, 0.1)
+
+    clientSocket.send(("0"*6+"1"*6+"2"*6+"3"*6+"4"*6+"5"*6+"6"*6+"7"*6+"8"*6+"9"*6)*10)
+    # clientSocket.send("000000111111222222333333")
+
     sleep(1)
-    serverSocket.lossy_udp_socket.stop()
-    clientSocket.lossy_udp_socket.stop()
+    serverSocket.sock.stop()
+    clientSocket.sock.stop()
--- Praktikum/Go-Back-N/lossy_udp_socket.py
+++ Praktikum/Go-Back-N/lossy_udp_socket.py
@@ -1,33 +1,35 @@
 import socket
+from time import sleep
 from threading import Thread
 import random
 
+
 class lossy_udp_socket():
-    nBytes=1500
-    
-    def __init__(self,conn,loc_port,rem_addr,PLR):
+    nBytes = 1500
+
+    def __init__(self, conn, loc_port, rem_addr, PLR):
         # conn: handler to be called for received packets with function "receive(packet)"
         # loc_port: local port
         # rem_addr: remote address and port pair
         # PLR: received packets are dropped with probability PLR
-        self.conn=conn
-        self.STOP=False
-        self.PLR=PLR
-        self.sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.conn = conn
+        self.STOP = False
+        self.PLR = PLR
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.settimeout(1.0)
-        self.sock.bind(('',loc_port))
-        self.addr=rem_addr
-        t=Thread(target=self.recv)
+        self.sock.bind(('', loc_port))
+        self.addr = rem_addr
+        t = Thread(target=self.recv)
         t.start()
-        
+
     # interface for sending packets
-    def send(self,packet):
-        print('Sending packet with length: '+str(len(packet)))                       
-        self.sock.sendto(packet,self.addr)
+    def send(self, packet):
+        #print('Sending packet with length: ' + str(len(packet)))
+        self.sock.sendto(packet, self.addr)
 
     # interface for ending socket
     def stop(self):
-        self.STOP=True
+        self.STOP = True
 
     # continuously listening for incoming packets
     # filters packets for remote address
@@ -35,14 +37,20 @@ class lossy_udp_socket():
     def recv(self):
         while not self.STOP:
             try:
-                packet,addr=self.sock.recvfrom(self.nBytes)
-                if addr==self.addr:
-                    if random.random()>self.PLR:
-                        print('Received packet with length: '+str(len(packet)))                       
+                packet, addr = self.sock.recvfrom(self.nBytes)
+                #print("addr: " + str(addr))
+                #print("self.addr: " + str(self.addr))
+                if addr == self.addr:
+                    if random.random() > self.PLR:
+                        #print('Received packet with length: ' + str(len(packet)))
                         self.conn.receive(packet)
                     else:
-                        print('Dropped packet with length: '+str(len(packet)))                       
+                        id = packet.decode("utf-8")[:4]
+                        while id.startswith("0") and len(id) > 1: id = id[1:]
+                        print('Dropped packet ' + id)
+                        sleep(0.001)
+
                 else:
-                    print('Warning: received packet from remote address'+str(addr))
+                    print('Warning: received packet from remote address' + str(addr))
             except socket.timeout:
                 pass
