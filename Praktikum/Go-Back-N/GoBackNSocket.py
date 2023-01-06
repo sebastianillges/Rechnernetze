34d4003a3f552ff0bf43ea16ba96b67db0df2a5b
sebastianillges <sebastian.illges@gmail.com>
2023-01-06 14:25:14 +0100

    bitte Kröte mich

--- Praktikum/Go-Back-N/GoBackNSocket.py
+++ Praktikum/Go-Back-N/GoBackNSocket.py
@@ -7,7 +7,8 @@ from math import ceil
 from lossy_udp_socket import lossy_udp_socket
 
 
-class go_back_n_socket():
+class GoBackNSocket():
+    currentSeg = 0
 
     def __init__(self, localPort, remotePort, remoteAddress, PLR=0.1, segmentSize=1000, windowSize=10):
         self.localPort = localPort
@@ -19,17 +20,22 @@ class go_back_n_socket():
 
 
     def send(self, msg):
-        len = len(msg)
-        numSeg = ceil(len/ self.segmentSize)
-        msgSegments = []
-        for i in range(numSeg):
-            msgSegments.append(msg[i * self.segmentSize: (i + 1) * self.segmentSize])
-            print('SOCK DEBUG | Message segments: ', msgSegments) if self.debugFlag else None
-
-        # Send the message segments
-        for i in range(numSeg):
-            print('SOCK DEBUG | Sending segment: ', msgSegments[i]) if self.debugFlag else None
-            self.sock.send(msgSegments[i].encode('utf-8'))
+        length = len(msg)
+        numSegments = ceil(length/ self.segmentSize)
+        segments = []
+        for i in range(0, numSegments):
+            segments.append(msg[i * self.segmentSize : (i+1) * self.segmentSize])
+        while(GoBackNSocket.currentSeg < numSegments)
 
     def receive(self, packet):
 
+if __name__ == '__main__':
+    serverPort = 12000
+    clientPort = 12001
+    serverSocket = GoBackNSocket(serverPort, clientPort, "localhost")
+
+    clientSocket = GoBackNSocket(clientPort, serverPort, "127.0.0.96")
+    clientSocket.send("ichbingay")
+    sleep(1)
+    serverSocket.lossy_udp_socket.stop()
+    clientSocket.lossy_udp_socket.stop()
